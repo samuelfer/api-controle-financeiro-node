8a14b6cca2293aec9c77c204ec645a64286e35ec
samuelfer <samuelfesant@gmail.com>
2022-12-23 15:15:25 -0300

    feat: add login

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require("express");
 const cors = require("cors");
 
 const AuthRegisterUserRoutes = require("./routes/AuthRegisterUserRoutes");
+const LoginRoutes = require("./routes/LoginRoutes");
+
 
 require("dotenv").config();
 
 var app = express();
+app.use(express.json());
 
 app.use(cors());
 
 app.use(AuthRegisterUserRoutes);
+app.use(LoginRoutes);
 
 const port = process.env.PORT || 3000;
 
---
+++ controllers/LoginController.js
@@ -0,0 +1,47 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
+module.exports = class LoginController {
+    static async login(req, res) {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.status(422).json({ message: "O email é obrigatório" });
+        }
+
+        if (!password) {
+            return res.status(422).json({ message: "A senha é obrigatória" });
+        }
+
+        let user = null;
+
+        try {
+            user = await User.findOne({ email: email });
+        } catch (error) {
+            console.error(error);
+            res.send("error", error)
+        }
+
+        if (!user) {
+            res.status(404).json({ message: "Usuário não encontrado" });
+        }
+
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if (!checkPassword) {
+            return res.status(422).json({ message: "Usuário ou Senha inválido" });
+        }
+
+        try {
+            const secret = process.env.SECRET;
+            const token = jwt.sign({ id: user._id }, secret);//Gera o token com base no id
+            res.status(200).json({ message: "Login realizado com sucesso", token });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Erro ao tentar realizar o login" });
+        }
+    }
+}
