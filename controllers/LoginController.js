ae75a1e5b06ee282afd4705f6b008f0d51092878
samuelfer <samuelfesant@gmail.com>
2022-12-23 17:27:10 -0300

    feat: add método para pesquisar por usuário

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -55,4 +55,16 @@ module.exports = class LoginController {
             console.error(error);
         });
     }
+
+    static async getUserById(req, res) {
+        const id = req.params.id;
+
+        const user = await User.findById(id, '-password');
+
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado" }); 
+        }
+        
+        res.status(200).json({ user });
+    }
 }
